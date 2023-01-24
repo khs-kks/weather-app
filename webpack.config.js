7d9fe14a647e3c57364c22ab2058cd8f44b2f1a2
khs <ksalumiev@gmail.com>
2023-01-24 20:07:49 +0100

    Add babel config

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,17 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  targets: {
+                    browsers: "0.25%",
+                    esmodules: true,
+                  },
+                },
+              ],
+            ],
           },
         },
       },
