38831d493fe78eb81429801031f01162827a832e
khs <ksalumiev@gmail.com>
2023-01-24 19:57:01 +0100

    Setup dev env

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "docs"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+};
