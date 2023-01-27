cd85905da3524054fcf0e8b13ed8fdd6a9b2ff57
khs <ksalumiev@gmail.com>
2023-01-27 17:28:17 +0100

    Finish styling the page

--- src/index.js
+++ src/index.js
@@ -4,4 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
   document.body.classList.add("show");
 });
 
+const modal = document.querySelector(".modal");
+const closeModal = document.querySelector(".close");
+
+closeModal.addEventListener("click", () => {
+  modal.style.display = "none";
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    modal.style.display = "none";
+  }
+});
+
+modal.addEventListener("click", (event) => {
+  if (event.target === modal) {
+    modal.style.display = "none";
+  }
+});
+
 Weather.getWeather("Landstuhl");
--- src/weather-class.js
+++ src/weather-class.js
@@ -1,17 +1,35 @@
 export default class Weather {
   static async getWeather(cityName) {
-    const coordinates = await fetch("../weather_testing/landstuhl-coords.json", {
-      mode: "cors",
-    });
-    const weatherPromise = await fetch("../weather_testing/landstuhl-data.json", {
-      mode: "cors",
-    });
+    const startTime = performance.now();
 
-    const weatherData = await weatherPromise.json()
-    
-    return new Weather(weatherData);
+    try {
+      const coordinates = await fetch(
+        "../weather_testing/landstuhl-coords.json",
+        {
+          mode: "cors",
+        }
+      );
+      const weatherPromise = await fetch(
+        "../weather_testing/landstuhl-data.json",
+        {
+          mode: "cors",
+        }
+      );
+
+      const weatherData = await weatherPromise.json();
+
+      const endTime = performance.now();
+      const timeTaken = (endTime - startTime) / 1000;
+
+      document.querySelector(".fetch-time").textContent = `Response time: ${timeTaken.toFixed(3)}s`  ;
+
+      return new Weather(weatherData);
+    } catch (error) {
+      console.log(error);
+      document.querySelector(".modal").style.display = "block";
+      document.querySelector(".error-code").textContent = error.code;
+    }
   }
-
 
   constructor(weatherCity) {
     this.temp = weatherCity.main.temp;
